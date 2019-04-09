437a0018012060ef063a7d8720d786bd9261188e
jese.alvarado@sv.cds <jese.alvarado@sv.cds>
2019-04-09 09:05:27 -0600

    Agregar archivos de proyecto.

---
+++ LocalDatabaseTwoTables/LocalDatabaseTwoTables/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LocalDatabaseTwoTables
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGuardar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
