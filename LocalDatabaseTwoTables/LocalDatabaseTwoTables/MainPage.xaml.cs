ab6c530a3830d9036eec33e1845b1087b49122cf
jese.alvarado@sv.cds <jese.alvarado@sv.cds>
2019-04-23 07:33:44 -0600

    Updated MainPage

--- LocalDatabaseTwoTables/LocalDatabaseTwoTables/MainPage.xaml.cs
+++ LocalDatabaseTwoTables/LocalDatabaseTwoTables/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel; // 
 using LocalDatabaseTwoTables.Model;
 using Xamarin.Forms;
+using System.Collections.Generic;
 
 namespace LocalDatabaseTwoTables
 {
@@ -29,7 +30,32 @@ namespace LocalDatabaseTwoTables
 
         private void BtnGuardar_Clicked(object sender, EventArgs e)
         {
+            string categoryName = CategoryNameEntry.Text;
+            Database db = new Database();
+            List<Category> categories = db.GetCategoriesWithChildren();
 
-        }
+           
+
+            Category newCategory = new Category();
+            newCategory.CategoryName = CategoryNameEntry.Text;
+            newCategory.Description = DescriptionEntry.Text;
+
+            newCategory.Products = new List<Product>();
+
+            // Recorrer la collection Products
+            foreach (Product product in Products)
+            {
+                newCategory.Products.Add(product);
+
+
+                // Instancia de Database
+             
+               Category Result = db.CreateCategoryWithChildren(newCategory);
+                if (Result.CategoryID > 0)
+                {
+                    lblMensaje.Text = "Ok";
+                }
+            }
+        }   
     }
 }
